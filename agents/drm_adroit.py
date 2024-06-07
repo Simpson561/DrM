fed888ca7ec1785bf99249c855284b509d100316
LazyJazz <zijian.lv@hotmail.com>
2024-06-07 10:09:03 +0800

    Difference encoder

--- agents/drm_adroit.py
+++ agents/drm_adroit.py
@@ -50,7 +50,7 @@ class Encoder(nn.Module):
         self.repr_dim = 32 * 35 * 35
 
         # print("obs_shape: ", obs_shape)
-        print("normal encoder")
+        print("difference encoder")
 
         self.convnet = nn.Sequential(nn.Conv2d(obs_shape[0], 32, 3, stride=2),
                                      nn.ReLU(), nn.Conv2d(32, 32, 3, stride=1),
@@ -61,10 +61,11 @@ class Encoder(nn.Module):
         self.apply(utils.weight_init)
 
     def forward(self, obs):
-        # print("obs.shape: ", obs.shape)
-        # obs[..., 1, :, :] -= obs[..., 0, :, :]
-        # obs[..., 2, :, :] -= obs[..., 0, :, :]
-        obs = obs / 255.0 - 0.5
+        obs = obs * 1.0
+        obs[..., 1, :, :] -= obs[..., 0, :, :]
+        obs[..., 2, :, :] -= obs[..., 0, :, :]
+        obs[..., 0, :, :] = obs[..., 0, :, :] / 255.0 - 0.5
+        # obs = obs / 255.0 - 0.5
         h = self.convnet(obs)
         h = h.view(h.shape[0], -1)
         return h
